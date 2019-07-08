a1384163ed818f6ab48b370f0add2ad70d0239cc
Brian Simonton <bsimonton78@gmail.com>
2019-07-08 06:21:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function Scooter(year, color, model){
 function Driver(name, age, experience){
   this.name = name;
   this.age = age;
-  this.model = model;
+  this.experience = experience;
   
 }
